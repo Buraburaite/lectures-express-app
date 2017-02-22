95340937add7c22911c71683df7a16d7624221de
Javier <jvrcueto@outlook.com>
2017-02-22 10:08:46 -0500

    first part done

--- app.js
+++ app.js
@@ -3,11 +3,26 @@ const express = require('express');
 // We create our own server named app
 // Express server handling requests and responses
 const app = express();
+app.use(express.static('public'));
 
 // our first Route
 app.get('/', (request, response, next) => {
   console.log(request);
   response.send('<p>Welcome Ironhacker. :)</p>');
+});
+
+app.get('/hello', (request, response, next) => {
+  response.send(`
+    <!doctype html>
+    <html>
+      <head>
+        <link rel="stylesheet" href="stylesheets/style.css">
+      </head>
+      <body>
+        This is my second route
+      </body>
+    </html>
+  `);
 });
 
 app.listen(3000, () => {
