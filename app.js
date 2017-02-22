f8e20f89dc988a51b9c6e95cd7f43d021ae52cad
Javier <jvrcueto@outlook.com>
2017-02-22 10:28:44 -0500

    made my own route

--- app.js
+++ app.js
@@ -4,15 +4,23 @@ const express = require('express');
 // Express server handling requests and responses
 const app = express();
 app.use(express.static('public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 // our first Route
-app.get('/', (request, response, next) => {
-  console.log(request);
-  response.send('<p>Welcome Ironhacker. :)</p>');
+app.get('/', (req, res, next) => {
+  // send views/index.ejs for displaying in the browser
+  res.render('index');
 });
 
-app.get('/hello', (request, response, next) => {
-  response.send(`
+// my second Route
+app.get('/about', (req, res, next) => {
+  // send views/index.ejs for displaying in the browser
+  res.render('about');
+});
+
+app.get('/hello', (req, res, next) => {
+  res.send(`
     <!doctype html>
     <html>
       <head>
